4f5d9df8f521bf80de0251a9e26820767fd55c9e
Ben Miles <ben.miles2@gmail.com>
2013-04-25 16:15:43 +0100

    no functionality tested just an update added methods for get and set rate.

--- Pump Controller/constantpressurepump control.rb
+++ Pump Controller/constantpressurepump control.rb
@@ -4,6 +4,15 @@
 
 require "serialport"
 
+def getRate
+ pumpport.write("crate")
+ pumpport.gets.chomp
+end
+
+def setRate(rate, units)
+ pumpport.write("irate [{#{rate}} {#{rate units}}]")
+end
+
 #params for pressure serial port
 port_str = "/dev/ttyUSB0"  #may be different for you
 baud_rate = 9600
@@ -11,22 +20,30 @@ data_bits = 8
 stop_bits = 1
 parity = SerialPort::NONE
 
-sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+pressureport = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+
+port_str = "/dev/ttyUSB0"  #may be different for you
+baud_rate = 9600
+data_bits = 8
+stop_bits = 1
+parity = SerialPort::NONE
+
+pumpport = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
 
 #continously running program
 while true do
   #measure pressure from Arduino connected to serial port.
-  while (pressure = sp.gets.chomp) do       # see note 2
+  while (pressure = pressureport.gets.chomp) do       # see note 2
     puts pressure
     #puts i.class #String
   end
     
   #Takes the pressure reading and increases or decreases infuse rate to obtain desired pressure
   if pressure < target_pressure
-    #serial write increase infuse rate (i need something here that picks how much the rate should change.)
+    pumpport.write ""#serial write increase infuse rate (i need something here that picks how much the rate should change.)
   else if pressure > target_pressure 
     #serial write decrease infuse rate
   end
 end
 
-sp.close                       #see note 1
+pressureport.close                       #see note 1
